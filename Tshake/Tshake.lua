e3018cb7714af8059605a1bc969a6315ad3fbaa0
ghost11886 <mumu2111999@yahoo.com>
2019-07-03 00:10:16 +0300

    Update Tshake.lua

--- Tshake/Tshake.lua
+++ Tshake/Tshake.lua
@@ -7515,7 +7515,7 @@ zhrf18 = zhrf18:gsub('ز', 'ز')
 zhrf18 = zhrf18:gsub('و', 'ﯛ̲୭')
 zhrf18 = zhrf18:gsub("ه", "໋۠هہؚ")
 
-zhrf6 = text:gsub('ض', 'ض͜ـ')
+zhrf19 = text:gsub('ض', 'ض͜ـ')
 zhrf19 = zhrf19:gsub('ص', 'ص͜ـ')
 zhrf19 = zhrf19:gsub('ث', 'ث͜ـ͜ـ')
 zhrf19 = zhrf19:gsub('ق', 'ق͜ـ')
